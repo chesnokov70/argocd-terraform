decd61b2914cd191f321420b2775bd29388b1ac3
chesnokov70 <164242795+chesnokov70@users.noreply.github.com>
2024-09-10 22:34:40 -0400

    Sc/lesson acd (#5)
    * terraform
    
    * manifests
    
    * manifests_replace

--- terraform/deploy_argocd.tf
+++ terraform/deploy_argocd.tf
@@ -16,14 +16,18 @@ module "argocd_prod" {
 module "argocd_dev_root" {
   source             = "./terraform_argocd_root_eks"
   eks_cluster_name   = "demo-dev"
+
   git_source_path    = "demo-dev/applications"
   git_source_repoURL = "git@github.com:adv4000/argocd.git"
+
 }
 
 # Can be deployed ONLY after ArgoCD deployment: depends_on = [module.argocd_prod]
 module "argocd_prod_root" {
   source             = "./terraform_argocd_root_eks"
   eks_cluster_name   = "demo-prod"
+
   git_source_path    = "demo-prod/applications"
   git_source_repoURL = "git@github.com:adv4000/argocd.git"
+
 }
