2ea94276cd6907acb5e122e96614481580130d7d
Sergei Chesnokov <ches.tranveks@gmail.com>
2024-09-16 21:04:47 -0400

    added all

--- terraform/deploy_argocd.tf
+++ terraform/deploy_argocd.tf
@@ -12,18 +12,18 @@ module "argocd_prod" {
 }
 
 
-# # Can be deployed ONLY after ArgoCD deployment: depends_on = [module.argocd_dev]
-# module "argocd_dev_root" {
-#   source             = "./terraform_argocd_root_eks"
-#   eks_cluster_name   = "demo-dev"
-#   git_source_path    = "demo-dev/applications"
-#   git_source_repoURL = "git@github.com:adv4000/argocd.git"
-# }
+# Can be deployed ONLY after ArgoCD deployment: depends_on = [module.argocd_dev]
+module "argocd_dev_root" {
+  source             = "./terraform_argocd_root_eks"
+  eks_cluster_name   = "demo-dev"
+  git_source_path    = "demo-dev/applications"
+  git_source_repoURL = "git@github.com:chesnokov70/argocd.git"
+}
 
-# # Can be deployed ONLY after ArgoCD deployment: depends_on = [module.argocd_prod]
-# module "argocd_prod_root" {
-#   source             = "./terraform_argocd_root_eks"
-#   eks_cluster_name   = "demo-prod"
-#   git_source_path    = "demo-prod/applications"
-#   git_source_repoURL = "git@github.com:adv4000/argocd.git"
-# }
+# Can be deployed ONLY after ArgoCD deployment: depends_on = [module.argocd_prod]
+module "argocd_prod_root" {
+  source             = "./terraform_argocd_root_eks"
+  eks_cluster_name   = "demo-prod"
+  git_source_path    = "demo-prod/applications"
+  git_source_repoURL = "git@github.com:chesnokov70/argocd.git"
+}
